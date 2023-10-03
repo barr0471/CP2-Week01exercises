e4a0d060eb164eb51300516f92977c2eea0287cc
Doug Barrett <dougbarrett@Dougs-MacBook-Pro.local>
2023-10-03 14:37:04 +1030

    Moved questions into functions, and completed second question.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+void blastoff();
+void output();
+
 int main() {
+
+    output();
+    return 0;
+}
+
+void output(){
+    using namespace std;
+
+    cout << "I was printed by C++" << endl;
+    cout << "\"I\" was printed by C++" << endl;
+    cout << "\"I\" was printed by \"cout << \\\"I\\\" << endl\"" << endl;
+}
+
+void blastoff(){
     for(int i= 10; i > 0; i--){
         std::cout << i << ", ";
     }
     std::cout << "Blastoff!";
-    return 0;
 }
